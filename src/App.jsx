696641d283ff7e55ea07af34dea5ca6202ca48c9
Noob-H4cker <alsamiul123@gmail.com>
2023-10-30 11:37:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  const x = "34";
+  console.log(x);
+
+  return (
+    <>
+      <div>
+        <h1>dasl</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
